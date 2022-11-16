d55e2b99841de5e617c89bdbec3c948cb5426c94
denashams <dena.shamsizadeh@gmail.com>
2022-11-16 16:57:02 +0330

    Added search engine

--- src/app.js
+++ src/app.js
@@ -40,7 +40,17 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
-let city = "Bushehr";
-let apiKey = "f62ff249a72da3b34cec98a655e25650";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "f62ff249a72da3b34cec98a655e25650";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
